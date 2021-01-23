6beb6e4462323d4fa5dd2482cabf9bcd76276c12
Brendan Bath <brendanbath@gmail.com>
2021-01-23 20:59:43 +0200

    Schema

--- src/main/scala/GraphQLSchema.scala
+++ src/main/scala/GraphQLSchema.scala
@@ -1,7 +1,4 @@
-import gateways.Rest.Polygon.ExchangeGateway
-import resolvers.StocksResolver
 import sangria.schema._
-import schemas.PolygonSchema
 import com.softwaremill.macwire._
 
 
@@ -9,8 +6,6 @@ import com.softwaremill.macwire._
   * Defines a GraphQL schema for the current project
   */
 object GraphQLSchema {
-  lazy val stocksSchema = wire[PolygonSchema]
-
   val query = ObjectType("Query", fields(List(): _*))
   val mutation = Some(
     ObjectType("Mutation", fields(List(): _*))
